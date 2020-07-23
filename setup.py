21ca6f2e54e518b206078d4745f2409da149f1de
vmmadathil <madathilvisakh@gmail.com>
2020-07-23 18:26:35 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploring the relationship between unemployment and health insurance coverage',
+    author='Visakh Madathil',
+    license='MIT',
+)
