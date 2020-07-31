30475142a12a234c68cc024136a03c0ead4eb541
vmmadathil <madathilvisakh@gmail.com>
2020-07-31 13:50:26 -0500

    UI scraper connects

--- src/data/getUIData.py
+++ src/data/getUIData.py
@@ -1,6 +1,7 @@
 import urllib.request
 import datetime
-
+import re
+import pandas as pd
 #starting date
 startDate = datetime.datetime.strptime("03/07/20", "%m/%d/%y")
 
@@ -12,12 +13,16 @@ currDate = startDate
 #iterating through 
 while (currDate <= endingDate):
     currString = (currDate.strftime("%m/%d/%y")).replace('/', '') 
-    page = urllib.request.urlopen("https://oui.doleta.gov/unemploy/page8/2020/" + currString + ".html")
-    pageBytes = page.read()
+    url = "https://oui.doleta.gov/unemploy/page8/2020/" + currString + ".html"
+    tables = pd.read_html(url, header=None)
+    table = tables[0]
 
-    pageStr = pageBytes.decode("utf8")
-    page.close()
+    table = table.iloc[0:, : 4]
+    table.columns = table.columns.droplevel([-1, -2, -3]) 
+    
+    table = table.rename(columns = {'STATENAME' : 'State', 'Initial Claims Filed During Week Ended  March 7, 2020' : 'Initial Claims', 'Initial Claims Filed During Week Ended  March 7, 2020' : 'Change From Last Week', 'Initial Claims Filed During Week Ended  March 7, 2020' : 'Change From Last Year'})
+    print(table)
 
-    print(pageStr)
+    
     currDate = currDate + datetime.timedelta(days=5)
 
